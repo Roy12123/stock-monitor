40f959e30dac2ed2c8f78146b2bebbc972ac9341
Chih-Li <roysmacbook@MacBook-Pro-4.local>
2025-11-10 20:09:31 +0800

    Fix wsgi.py: expose app variable for Gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from app import app, socketio
+import os
+
+# For Gunicorn
+application = app
 
 if __name__ == "__main__":
-    socketio.run(app)
+    port = int(os.environ.get('PORT', 5005))
+    socketio.run(app, host='0.0.0.0', port=port)
